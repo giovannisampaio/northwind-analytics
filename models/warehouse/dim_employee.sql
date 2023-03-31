3883dfae50c71ec602df090373713f550f8e7239
giovannisampaio <giovannibruno1@gmail.com>
2023-03-31 19:07:21 +0100

    feat: finish warehouse layer

--- models/warehouse/dim_employee.sql
+++ models/warehouse/dim_employee.sql
@@ -1,6 +1,6 @@
 with source as (
    select
-      id as customer_id,
+      id as employee_id,
       company,
       last_name,
       first_name,
@@ -19,11 +19,11 @@ with source as (
       notes,
       attachments,
       current_timestamp() as ingestion_timestamp
-   from {{ ref('stg_customer') }}
+   from {{ ref('stg_employees') }}
 ),
 unique_source as (
    select *,
-            row_number() over(partition by customer_id) as rn 
+            row_number() over(partition by employee_id) as rn 
    from source
 )
 select *
