d87d801f2d65ed405f19a330b663d8d3e0d08dd0
giovannisampaio <giovannibruno1@gmail.com>
2023-03-31 17:29:07 +0100

    start warehouse layer

---
+++ models/warehouse/dim_customer.sql
@@ -0,0 +1,21 @@
+select
+    unique_customer_id,
+    company,
+    last_name,
+    first_name,
+    email_address,
+    job_title,
+    business_phone,
+    home_phone,
+    mobile_phone,
+    fax_number,
+    address,
+    city,
+    state_province,
+    zip_postal_code,
+    country_region,
+    web_page,
+    notes,
+    attachments,
+    current_timestamp() as ingestion_timestamp
+from {{ ref('stg_customer') }}
